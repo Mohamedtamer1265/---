df60c034b96fa747eb4e81f048cb14e0a2cc367d
Mohamed Tamer <83618615+Mohamedtamer1265@users.noreply.github.com>
2021-07-22 20:40:33 +0200

    Add files via upload

---
+++ sb7a/js/sp7a.js
@@ -0,0 +1,20 @@
+let currentItem = 0;
+let btns = document.querySelectorAll(".btn");
+let number = document.querySelector(".number");
+
+btns.forEach(function (element){
+    element.addEventListener("click", function (e){
+        let target = e.currentTarget.classList;
+        if(target.contains('increase')){
+            currentItem++
+        }else if(target.contains("reset")) {
+         currentItem = 0
+        }
+        number.textContent = currentItem;
+    })
+})
+document.querySelector(".increase").addEventListener("click", function(numb){
+    navigator.vibrate(numb)
+    numb = 1000;
+})
+    
