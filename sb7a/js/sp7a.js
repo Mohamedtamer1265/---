d117f3df9b3d7defcb90de6c4820d8a864ad66a9
Mohamed Tamer <83618615+Mohamedtamer1265@users.noreply.github.com>
2021-07-26 00:38:37 +0200

    Add files via upload

--- sb7a/js/sp7a.js
+++ sb7a/js/sp7a.js
@@ -15,6 +15,6 @@ btns.forEach(function (element){
 });
 document.querySelector(".increase").addEventListener("click", function vibrate()
 {
-    navigator.vibrate(5);
+    navigator.vibrate(2);
 })
     
